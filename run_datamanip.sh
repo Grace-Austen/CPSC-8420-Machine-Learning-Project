5d6febce1b7a79744b1cf5fe80f19213a04383c4
Grace-Austen <gausten@clemson.edu>
2023-12-02 11:30:36 -0500

    Added a bash file to run datamanip

---
+++ run_datamanip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#PBS -N datamanip
+#PBS -l select=1:ncpus=10:mem=10gb:interconnect=1g
+#PBS -l walltime=3:00:00
+#PBS -o datamanip_output.txt
+#PBS -j oe
+
+module load matlab/2022a
+matlab -batch "datamanip"
