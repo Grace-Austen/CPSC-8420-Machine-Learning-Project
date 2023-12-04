59d480afaef45fc2f2ae91f76812b6160c5cd76d
Grace-Austen <gausten@clemson.edu>
2023-12-03 22:00:41 -0500

    Fix bash again?

--- train_no_pca_models.sh
+++ train_no_pca_models.sh
@@ -9,6 +9,6 @@
 
 module load matlab/2022a
 cd CPSC-8420-Machine-Learning-Project
-matlab -r 'train_model(model="linear", pca=0, name_data_file="data/50k_data_name.mat", descript_data_file="data/50k_data_descript.mat", other_data_file="data/50k_data_other.mat"); exit' -logfile linear_no_pca_model_train.txt
-matlab -r 'train_model(model="ridge", pca=0, name_data_file="data/50k_data_name.mat", descript_data_file="data/50k_data_descript.mat", other_data_file="data/50k_data_other.mat"); exit' -logfile ridge_no_pca_model_train.txt
-matlab -r 'train_model(model="lasso", pca=0, name_data_file="data/50k_data_name.mat", descript_data_file="data/50k_data_descript.mat", other_data_file="data/50k_data_other.mat"); exit' -logfile lasso_no_pca_model_train.txt
+matlab -r "train_model(model=\"linear\", pca=0, name_data_file=\"data/50k_data_name.mat\", descript_data_file=\"data/50k_data_descript.mat\", other_data_file="data/50k_data_other.mat"); exit" -logfile linear_no_pca_model_train.txt
+matlab -r "train_model(model=\"ridge\", pca=0, name_data_file=\"data/50k_data_name.mat\", descript_data_file=\"data/50k_data_descript.mat\", other_data_file="data/50k_data_other.mat"); exit" -logfile ridge_no_pca_model_train.txt
+matlab -r "train_model(model=\"lasso\", pca=0, name_data_file=\"data/50k_data_name.mat\", descript_data_file=\"data/50k_data_descript.mat\", other_data_file="data/50k_data_other.mat"); exit" -logfile lasso_no_pca_model_train.txt
