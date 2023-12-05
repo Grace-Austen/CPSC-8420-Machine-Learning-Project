3ce49c935671e783f969262e7f4633da324ab972
jcschec <43623669+jcschec@users.noreply.github.com>
2023-12-04 20:33:56 -0500

    Add files via upload

--- pca_noRegression.m
+++ pca_noRegression.m
@@ -2,7 +2,7 @@ function scores = pca_noRegression(X)
     % X = X(:, 1:10)
     % mu = mean(X);
     % X_cent = X - mu;
-    X_cent = X(:, 1:10); % PROBLEM: There is no distinct elbow point with just the one_hot_(descript or name) but is with
+    X_cent = X; % PROBLEM: There is no distinct elbow point with just the one_hot_(descript or name) but is with
                          %          pca_noRegression([indicator_data one_hot_name]) at K = 2  
 
     [~, S, V] = svd(cov(X_cent));
@@ -12,11 +12,14 @@ function scores = pca_noRegression(X)
     scores = X_cent * V;
 
     figure('Position', [100, 100, 800, 400]);
+    % total_var = sum(diag(S));
+    % exp_var = cumsum(diag(S)) / total_var;
     total_var = sum(diag(S));
-    exp_var = cumsum(diag(S)) / total_var;
+    var = diag(S)/total_var;
+    exp_var = cumsum(var);
     
     % Scree Plot
-    plot(1:length(exp_var),(1 - exp_var), 'o-', 'LineWidth', 2);
+    plot(1:length(exp_var),(var), 'o-', 'LineWidth', 2);
     title('Scree Plot');
     xlabel('Principal Component');
     ylabel('Proportion of Explained Variance');
